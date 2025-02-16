4e4127820b57b8fe07a208519d9028ee1830380f
Dht <1362073193@qq.com>
2025-02-16 17:52:55 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -64,3 +64,12 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+
+@app.route('/api/wechat', methods=['GET'])
+def wechat():
+    '''
+    微信接口
+    '''
+
+    return 'success'
